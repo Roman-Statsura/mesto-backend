25ed42a376861d0998f94aac65d4f7c6a156c3c2
Roman-Statsura <srs1012@yandex.ru>
2020-09-08 17:36:03 +0400

    made small fixes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,18 @@
 const jwt = require('jsonwebtoken');
+const Unauthorized = require('../errors/unauthorized');
 const { secretKey } = require('../secretKey');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   if (!req.cookies.jwt) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    next(new Unauthorized('Необходима авторизация'));
   }
   const token = req.cookies.jwt;
   let payload;
   try {
     payload = jwt.verify(token, secretKey);
   } catch (err) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    next(new Unauthorized('Необходима авторизация'));
   }
   req.user = payload;
 
