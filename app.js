4f7ce497356ce8f0a4ab1ca73dec54792be48595
Roman-Statsura <srs1012@yandex.ru>
2020-09-14 15:44:19 +0400

    fixed cors1

--- app.js
+++ app.js
@@ -30,14 +30,14 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 const whitelist = ['https://www.api.wantcarrots.tk', 'https://api.wantcarrots.tk', 'http://www.api.wantcarrots.tk', 'http://api.wantcarrots.tk', 'localhost:3000'];
 const corsOptions = {
-  origin(origin, callback) {
+  origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
+      callback(null, true)
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error('Not allowed by CORS'))
     }
-  },
-};
+  }
+}
 
 app.use(cors(corsOptions));
 
